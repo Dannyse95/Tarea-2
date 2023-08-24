78e5d1ad4d7b210a87e1e35e673e90c5189cebd5
Dannyse95 <141385738+Dannyse95@users.noreply.github.com>
2023-08-24 05:17:23 -0600

    Add files via upload

---
+++ Progamall.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vistal;
+
+/**
+ *
+ * @author augus
+ */
+public class Progamall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        program_fr frm = new program_fr();
+        frm.show();
+
+        
+        // TODO code application logic here
+    }
+    
+}
